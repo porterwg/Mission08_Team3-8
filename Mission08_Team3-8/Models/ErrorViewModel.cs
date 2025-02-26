037bdd6ea1f2cfe0da44e6ba28e1939af0623a69
porterwg <porterwg@byu.edu>
2025-02-26 14:08:06 -0700

    Initial commit

---
+++ Mission08_Team3-8/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team3_8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
